dd4086892b0684ec6c6c3ccedb80d7b49579d3cb
GiorgiKumelashvili <giorgi.kumelashvili21@gmail.com>
2024-06-13 18:48:20 +0400

    initialized shadcn + dark theme

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,10 @@ import ReactDOM from 'react-dom/client';
 import { App } from './App';
 
 import './index.css';
+import { ThemeProvider } from '@/components/theme-provider';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+    <App />
+  </ThemeProvider>
+);
